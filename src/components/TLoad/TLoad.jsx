dbaa1dfbc8723e388b0ae7ba8f7c15ee9c5e8e88
Tsvetus <Tsvetus@kdpmc.ru>
2020-02-05 19:49:59 +0300

    Updated TLoad component

--- src/components/TLoad/TLoad.jsx
+++ src/components/TLoad/TLoad.jsx
@@ -7,13 +7,32 @@ import styles from '../../styles';
 
 class TLoad extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.cursor = document.body.style.cursor;
+    }
+
+    componentWillUnmount() {
+        document.body.style.cursor = this.cursor;
+    }
+
     render () {
-        let style = merge(styles.TLoad, this.props.style, this.props.show ? {} : {display: "none"});
+
+        let style = merge(styles.TLoad, this.props.style, this.props.show &&
+            this.props.showCaption ? {} : {display: "none"});
+
+        if (this.props.show) {
+            document.body.style.cursor = "wait";
+        } else {
+            document.body.style.cursor = this.cursor;
+        }
+
         return (
             <div style={style}>
                 {this.props.show ? this.props.caption : null}
             </div>
         );
+
     }
 
 }
@@ -21,10 +40,12 @@ class TLoad extends React.Component {
 TLoad.propTypes = {
     style: PropTypes.object,
     caption: PropTypes.string,
+    showCaption: PropTypes.any,
     show: PropTypes.any
 };
 
 TLoad.defaultProps = {
+    showCaption: true,
     caption: 'Loading...'
 };
 
