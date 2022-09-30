ae57da8e0bb7addd97544f67a7c9617d17e12999
Tsvetus <Tsvetus@kdpmc.ru>
2022-09-30 15:57:49 +0300

    Added number and float regular expressions to util

--- src/util/index.js
+++ src/util/index.js
@@ -1,129 +1,112 @@
 import {
-    download,
-    clone,
-    nvl,
-    merge,
-    seconds,
-    apply,
-    find,
-    strip,
-    params,
-    scriptParams,
-    contain,
-    getFile,
-    compare,
-    replace,
-    isMS
-} from './misc.js';
+  download,
+  clone,
+  nvl,
+  merge,
+  seconds,
+  apply,
+  find,
+  strip,
+  params,
+  scriptParams,
+  contain,
+  getFile,
+  compare,
+  replace,
+  isMS,
+  toObj
+} from './misc.js'
+
+import { parseField, parseItem, Helper } from './helper.js'
+
+import Format from './format.js'
+import Pager from './pager.js'
+import Sizer from './sizer.js'
+import Uploader from './uploader.js'
+import Provider from './provider.js'
+
+import { TIMEOUT, REGEXP } from './const.js'
 
 import {
-    parseField,
-    parseItem,
-    Helper
-} from './helper.js';
+  isoDate,
+  strDate,
+  testIsoDate,
+  isoTime,
+  strTime,
+  testIsoTime,
+  cutDate,
+  cutTime,
+  firstDate,
+  lastDate,
+  clearDate
+} from './date.js'
 
-import Format from './format.js';
-import Pager from './pager.js';
-import Sizer from './sizer.js';
-import Uploader from './uploader.js';
-import Provider from './provider.js';
-
-import {
-    TIMEOUT,
-    REGEXP
-} from './const.js';
-
-import {
-    isoDate,
-    strDate,
-    testIsoDate,
-    isoTime,
-    strTime,
-    testIsoTime,
-    cutDate,
-    cutTime,
-    firstDate,
-    lastDate,
-    clearDate
-} from './date.js';
+import { getCookie, setCookie, deleteCookie } from './cookie.js'
 
 import {
-    getCookie,
-    setCookie,
-    deleteCookie
-} from './cookie.js';
-
-import {
-    post,
-    get,
-    request,
-    login,
-    logout,
-    check,
-    translate,
-    clear,
-    reducer,
-    hasAuth,
-    INITIAL_STATE
-} from './request.js';
+  post,
+  get,
+  request,
+  login,
+  logout,
+  check,
+  translate,
+  clear,
+  reducer,
+  hasAuth,
+  INITIAL_STATE
+} from './request.js'
 
 export {
-
-    download,
-    clone,
-    nvl,
-    merge,
-    seconds,
-    apply,
-    find,
-    strip,
-    params,
-    scriptParams,
-    contain,
-    getFile,
-    compare,
-    replace,
-    isMS,
-
-    parseField,
-    parseItem,
-    Helper,
-
-    Format,
-    Pager,
-    Sizer,
-    Uploader,
-    Provider,
-
-    isoDate,
-    strDate,
-    testIsoDate,
-    isoTime,
-    strTime,
-    testIsoTime,
-    cutDate,
-    cutTime,
-    firstDate,
-    lastDate,
-    clearDate,
-
-    getCookie,
-    setCookie,
-    deleteCookie,
-
-    TIMEOUT,
-    REGEXP,
-
-    post,
-    get,
-    request,
-    login,
-    logout,
-    check,
-    translate,
-    clear,
-    reducer,
-    hasAuth,
-    INITIAL_STATE
-
+  download,
+  clone,
+  nvl,
+  merge,
+  seconds,
+  apply,
+  find,
+  strip,
+  params,
+  scriptParams,
+  contain,
+  getFile,
+  compare,
+  replace,
+  isMS,
+  toObj,
+  parseField,
+  parseItem,
+  Helper,
+  Format,
+  Pager,
+  Sizer,
+  Uploader,
+  Provider,
+  isoDate,
+  strDate,
+  testIsoDate,
+  isoTime,
+  strTime,
+  testIsoTime,
+  cutDate,
+  cutTime,
+  firstDate,
+  lastDate,
+  clearDate,
+  getCookie,
+  setCookie,
+  deleteCookie,
+  TIMEOUT,
+  REGEXP,
+  post,
+  get,
+  request,
+  login,
+  logout,
+  check,
+  translate,
+  clear,
+  reducer,
+  hasAuth,
+  INITIAL_STATE
 }
--- src/util/misc.js
+++ src/util/misc.js
@@ -1,246 +1,251 @@
-import React from 'react';
+import React from 'react'
 
 export function merge(...sources) {
-    let res = {};
-    for (const source of sources) {
-        if (source instanceof Array) {
-            if (!(res instanceof Array)) {
-                res = []
-            }
-            source.forEach((v, i) => {res[i] = v});
-        } else if (source instanceof Object) {
-            for (let [key, value] of Object.entries(source)) {
-                let r = null;
-                if (value instanceof Object && key in res) {
-                    r = {[key]: merge(res[key], value)}
-                } else {
-                    r = {[key]: value};
-                }
-                res = {
-                    ...res,
-                    ...r
-                }
-            }
+  let res = {}
+  for (const source of sources) {
+    if (source instanceof Array) {
+      if (!(res instanceof Array)) {
+        res = []
+      }
+      source.forEach((v, i) => {
+        res[i] = v
+      })
+    } else if (source instanceof Object) {
+      for (let [key, value] of Object.entries(source)) {
+        let r = null
+        if (value instanceof Object && key in res) {
+          r = { [key]: merge(res[key], value) }
+        } else {
+          r = { [key]: value }
         }
+        res = {
+          ...res,
+          ...r
+        }
+      }
     }
-    return res;
+  }
+  return res
 }
 
 export function apply(from, to, style) {
+  if (!from || !to || !style) {
+    return
+  }
 
-    if (!from || !to || !style) {
-        return;
+  let f = Object.keys(from)
+  let t = Object.keys(to)
+
+  for (let i = 0; i < t.length; i++) {
+    if (to[t[i]] === undefined) {
+      style[t[i]] = null
+    } else {
+      style[t[i]] = to[t[i]]
     }
+  }
 
-    let f = Object.keys(from);
-    let t = Object.keys(to);
-
-    for (let i=0; i<t.length; i++) {
-        if (to[t[i]] === undefined) {
-            style[t[i]] = null;
-        } else {
-            style[t[i]] = to[t[i]];
-        }
+  if (from) {
+    for (let i = 0; i < f.length; i++) {
+      if (t.indexOf(f[i]) < 0) {
+        style[f[i]] = null
+      }
     }
-
-    if (from) {
-        for (let i=0; i<f.length; i++) {
-            if (t.indexOf(f[i]) < 0) {
-                style[f[i]] = null;
-            }
-        }
-    }
-
+  }
 }
 
 export function nvl(source, def) {
-    if (source === null || source === undefined) {
-        return def;
-    } else {
-        return source;
-    }
+  if (source === null || source === undefined) {
+    return def
+  } else {
+    return source
+  }
 }
 
 export function clone(source, exclude) {
-    let dest = null;
-    if (typeof source === 'function') {
-        dest = source;
-    } else if (source instanceof Array) {
-        dest = source.slice();
-        for (let i=0; i<dest.length; i++) {
-            dest[i] = clone(dest[i], exclude);
-        }
-    } else if (React.isValidElement(source)) {
-        dest = source;
-    } else if (source instanceof Date) {
-        dest = new Date(source.getTime());
-    } else if (source instanceof Object) {
-        dest = {};
-        let keys = Object.keys(source);
-        for (let i=0; i<keys.length; i++) {
-            if (exclude && exclude.indexOf(keys[i]) >= 0) {
-                continue
-            }
-            dest[keys[i]] = clone(source[keys[i]], exclude);
-        }
-    } else {
-        dest = source;
+  let dest = null
+  if (typeof source === 'function') {
+    dest = source
+  } else if (source instanceof Array) {
+    dest = source.slice()
+    for (let i = 0; i < dest.length; i++) {
+      dest[i] = clone(dest[i], exclude)
     }
-    return dest;
+  } else if (React.isValidElement(source)) {
+    dest = source
+  } else if (source instanceof Date) {
+    dest = new Date(source.getTime())
+  } else if (source instanceof Object) {
+    dest = {}
+    let keys = Object.keys(source)
+    for (let i = 0; i < keys.length; i++) {
+      if (exclude && exclude.indexOf(keys[i]) >= 0) {
+        continue
+      }
+      dest[keys[i]] = clone(source[keys[i]], exclude)
+    }
+  } else {
+    dest = source
+  }
+  return dest
 }
 
 export function download(url, filename) {
-    let link = document.createElement("a");
-    if (filename) {
-        link.download = filename;
-    }
-    link.target = "_blank";
-    link.href = url;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  let link = document.createElement('a')
+  if (filename) {
+    link.download = filename
+  }
+  link.target = '_blank'
+  link.href = url
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
 }
 
 export function seconds(source) {
-    return Date.parse('1970 ' + source + ' GMT')/1000;
+  return Date.parse('1970 ' + source + ' GMT') / 1000
 }
 
 export function find(node, parent) {
-    if (!node) {
-        return null;
+  if (!node) {
+    return null
+  }
+  if (node === parent) {
+    return parent
+  }
+  if (parent && parent.children) {
+    for (let i = 0; i < parent.children.length; i++) {
+      let child = parent.children[i]
+      if (node === child) {
+        return child
+      }
+      let found = find(node, child)
+      if (found) {
+        return found
+      }
     }
-    if (node === parent) {
-        return parent;
-    }
-    if (parent && parent.children) {
-        for (let i=0; i<parent.children.length; i++) {
-            let child = parent.children[i];
-            if (node === child) {
-                return child
-            }
-            let found = find(node, child);
-            if (found) {
-                return found;
-            }
-        }
-    }
-    return null;
+  }
+  return null
 }
 
 export function strip(source) {
-    if (source && typeof source === 'string') {
-        return source
-            .replace(/<br>/gm, '\r')
-            .replace(/<[^>]*>?/gm, '')
-            .replace(/&(.*?);+/gm, ' ');
-    } else {
-        return source;
-    }
+  if (source && typeof source === 'string') {
+    return source
+      .replace(/<br>/gm, '\r')
+      .replace(/<[^>]*>?/gm, '')
+      .replace(/&(.*?);+/gm, ' ')
+  } else {
+    return source
+  }
 }
 
 export function params(source) {
-    let res = {};
-    let path = source ? source : window.location.href;
-    if (path) {
-        let arr = path.split('?');
-        if (arr.length > 1) {
-            let query = arr[1];
-            if (query) {
-                let q = query.split('&');
-                q.forEach(param => {
-                    if (param) {
-                        let p = param.split('=');
-                        res[p[0]] = p[1];
-                    }
-                });
-            }
-        }
+  let res = {}
+  let path = source ? source : window.location.href
+  if (path) {
+    let arr = path.split('?')
+    if (arr.length > 1) {
+      let query = arr[1]
+      if (query) {
+        let q = query.split('&')
+        q.forEach(param => {
+          if (param) {
+            let p = param.split('=')
+            res[p[0]] = p[1]
+          }
+        })
+      }
     }
-    return res;
+  }
+  return res
 }
 
 export function scriptParams(scriptName) {
-    let scripts = document.getElementsByTagName("script");
-    for (let i=0; i<scripts.length; i++) {
-        if (scripts[i].src.indexOf("/" + scriptName) >= 0) {
-            return params(scripts[i].src);
-        }
+  let scripts = document.getElementsByTagName('script')
+  for (let i = 0; i < scripts.length; i++) {
+    if (scripts[i].src.indexOf('/' + scriptName) >= 0) {
+      return params(scripts[i].src)
     }
-    return {};
+  }
+  return {}
 }
 
 export function contain(source) {
-    if (source) {
-        let res = clone(source);
-        if (!res.container) {
-            res.container = {};
-        }
-        for (let key in source) {
-            if (!(source[key] instanceof Object)) {
-                res.container[key] = source[key];
-                delete res[key];
-            }
-        }
-        return res;
-    } else {
-        return source;
+  if (source) {
+    let res = clone(source)
+    if (!res.container) {
+      res.container = {}
     }
+    for (let key in source) {
+      if (!(source[key] instanceof Object)) {
+        res.container[key] = source[key]
+        delete res[key]
+      }
+    }
+    return res
+  } else {
+    return source
+  }
 }
 
-export function getFile(url, callback){
-    let request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.send(null);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            callback(request.responseText);
-        }
+export function getFile(url, callback) {
+  let request = new XMLHttpRequest()
+  request.open('GET', url, true)
+  request.send(null)
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(request.responseText)
     }
+  }
 }
 
 export function compare(foo, bar) {
-    if (foo instanceof Array && bar instanceof Array) {
-        let m = foo.length > bar.length ? foo.length : bar.length;
-        for (let i=0; i<m; i++) {
-            if (!compare(foo[i], bar[i])) {
-                return false;
-            }
-        }
-    } else if (foo instanceof Object && bar instanceof Object) {
-        for (let key in foo) {
-            if (!compare(foo[key], bar[key])) {
-                return false;
-            }
-        }
-        for (let key in bar) {
-            if (!compare(bar[key], foo[key])) {
-                return false;
-            }
-        }
-    } else {
-        if (foo !== bar) {
-            return false;
-        }
+  if (foo instanceof Array && bar instanceof Array) {
+    let m = foo.length > bar.length ? foo.length : bar.length
+    for (let i = 0; i < m; i++) {
+      if (!compare(foo[i], bar[i])) {
+        return false
+      }
     }
-    return true;
+  } else if (foo instanceof Object && bar instanceof Object) {
+    for (let key in foo) {
+      if (!compare(foo[key], bar[key])) {
+        return false
+      }
+    }
+    for (let key in bar) {
+      if (!compare(bar[key], foo[key])) {
+        return false
+      }
+    }
+  } else {
+    if (foo !== bar) {
+      return false
+    }
+  }
+  return true
 }
 
 export function replace(source, name, value) {
-    if ((typeof source) === 'string') {
-        let r = new RegExp('\\$\\{' + name + '\\}','gm');
-        return source.replace(r, value);
-    } else if ((typeof source) === 'object') {
-        for (let key in source) {
-            source[key] = replace(source[key], name, value);
-        }
-        return source;
-    } else {
-        return source;
+  if (typeof source === 'string') {
+    let r = new RegExp('\\$\\{' + name + '\\}', 'gm')
+    return source.replace(r, value)
+  } else if (typeof source === 'object') {
+    for (let key in source) {
+      source[key] = replace(source[key], name, value)
     }
+    return source
+  } else {
+    return source
+  }
 }
 
 export function isMS() {
-    let ua = window.navigator.userAgent;
-    return /MSIE|Trident|Edge/.test(ua);
+  let ua = window.navigator.userAgent
+  return /MSIE|Trident|Edge/.test(ua)
 }
 
+export function toObj(json) {
+  if (!json) return {}
+  const obj = JSON.parse(json)
+  return 'string' === typeof obj ? JSON.parse(obj) : obj
+}
